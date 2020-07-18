71fd3c59ead02d140ff5c1a618255b4c807f487c
김문기 <heartboy08@nate.com>
2020-07-18 12:07:09 +0900

    Glide 로 웹상의 이미지 가져오기

--- app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.library_20200718
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -24,5 +25,8 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
         //TODO("Not yet implemented")
+        val imageUrl = "https://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg"
+
+        Glide.with(mContext).load(imageUrl).into(profileImg)
     }
 }
