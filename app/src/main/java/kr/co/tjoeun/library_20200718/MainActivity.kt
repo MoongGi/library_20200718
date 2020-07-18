4a298cc446e259aa3b35f92fcfca96998e5db8e9
김문기 <heartboy08@nate.com>
2020-07-18 11:23:32 +0900

    포토뷰 라이브러리 사용

--- app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
@@ -1,12 +1,28 @@
 package kr.co.tjoeun.library_20200718
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        //TODO("Not yet implemented")
+        goToPhotoViewBtn.setOnClickListener {
+            val myIntent = Intent(mContext, ProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+
+    override fun setValues() {
+        //TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/kr/co/tjoeun/library_20200718/ProfilePhotoActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.library_20200718
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_photo)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+     //   TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+       // TODO("Not yet implemented")
+    }
+}
